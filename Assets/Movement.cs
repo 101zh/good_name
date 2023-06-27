95d921a2170712c333adb772257df74ca89dfa27
Hunter Zhuang <hunterzhuang0@gmail.com>
2023-06-27 12:07:21 -0700

    finished movement; trying to make clean movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,23 +5,19 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D rb;
+    [SerializeField] float moveSpeed;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
         Debug.Log("Hunter is stinky");
     }
 
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetKey("up"))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y*2);
-        }
-
-        if (Input.GetKey("down"))
-        {
-            rb.velocity = new vector2(rb.velocity.x, rb.velocity.y*-2);
-        }
+        float dirX = Input.GetAxis("Horizontal");
+        float dirY = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(moveSpeed*dirX, moveSpeed*dirY);
     }
 }
