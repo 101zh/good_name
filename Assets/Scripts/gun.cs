9ef372a3318fd956d013689d70396dcb8d5a4864
Hunter Zhuang <hunterzhuang0@gmail.com>
2023-06-28 11:48:57 -0700

    made the rotation of a gun a function
    - done for readability
    - called gunRotate()

--- Assets/Scripts/gun.cs
+++ Assets/Scripts/gun.cs
@@ -15,8 +15,18 @@ public class gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        gunRotate();
+
+    }
+
+    private void gunRotate()
+    {
+        // finds the position of the mouse using camera (has to be relative to it) and position of gun
         Vector2 dir = mainCam.ScreenToWorldPoint(Input.mousePosition)-transform.position;
+        // Finding the angle to rotate using math
         float angle = Mathf.Atan2(dir.y, dir.x)*Mathf.Rad2Deg;
+        // Rotates the gun using math
         transform.rotation = Quaternion.AngleAxis(angle,Vector3.forward);
     }
 }
