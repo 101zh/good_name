48c07f02917bd21ae1a69a6028df77bc242e39cf
Hunter Zhuang <hunterzhuang0@gmail.com>
2023-06-30 16:41:42 -0700

    renamed "gun.cs" to "gun_pos.cs"
    - prolly gonna create a new file for shooting the bullet out

--- Assets/Scripts/gun_pos.cs
+++ Assets/Scripts/gun_pos.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gun : MonoBehaviour
+public class gun_pos : MonoBehaviour
 {
     // Start is called before the first frame update
 
