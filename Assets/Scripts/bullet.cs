069138032fa4e02e6d1199a39f307c0cf2bf8088
Hunter Zhuang <hunterzhuang0@gmail.com>
2023-07-02 11:48:49 -0700

    worked on bullet collision; bugged
    - destroy doesn't work

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+     void OnCollisionEnter2D(Collision2D collision)
+     {
+        Destroy(this);
+     }
+
 }
