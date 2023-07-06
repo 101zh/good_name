6ea380ccb161c6614948390012388e638afa60c5
Waseem Munir <xxwaseemmunirxx@gmail.com>
2023-07-06 11:28:00 -0700

    Worked on knockback, just needs HP scripts

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Knockback : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private float strength = 16, delay = 0.15f;
+    public UnityEvent OnBegin, OnDone;
+
+    public void PlayFeedback(GameObject sender)
+    {
+        StopAllCoroutines();
+        OnBegin?.Invoke();
+        Vector2 direction = (transform.position - sender.transform.position).normalized;
+        rb2d.AddForce(direction*strength, ForceMode2D.Impulse);
+        StartCoroutine(Reset());
+    }
+    private IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(delay);
+        rb2d.velocity = new Vector3(0, 0, 0);
+        OnDone?.Invoke();
+
+    }
+}
--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -7,7 +7,8 @@ public class bullet : MonoBehaviour
 
      void OnCollisionEnter2D(Collision2D collision)
      {
-        Destroy(this);
+        Destroy(gameObject);
+
      }
 
 }
