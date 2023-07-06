9b3e9712b1bddabd6f04f7e6cafb8bf737aaf58d
Hunter Zhuang <hunterzhuang0@gmail.com>
2023-07-06 16:10:11 -0700

    made camera following more smoother
    - SmoothDamp(), instead of lerp()

--- Assets/Scripts/camera_controller.cs
+++ Assets/Scripts/camera_controller.cs
@@ -6,12 +6,13 @@ public class camera_controller : MonoBehaviour
     public Transform target;
     public float smoothSpeed;
     public Vector3 offset;
-
+    private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
     {
         Vector3 desiredPos=target.position+offset;
-        Vector3 smoothedPos= Vector3.Lerp(transform.position, desiredPos, smoothSpeed*Time.deltaTime);
+        desiredPos.z=transform.position.z;
+        Vector3 smoothedPos= Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothSpeed);
         transform.position= smoothedPos;
     }
 
