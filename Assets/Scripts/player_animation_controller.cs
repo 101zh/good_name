5e68397a74676203f1fc44b37a44ffa24591d8c7
Hunter Zhuang <hunterzhuang0@gmail.com>
2023-07-11 16:39:26 -0700

    witch has a walk animation now

---
+++ Assets/Scripts/player_animation_controller.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_animation_controller : MonoBehaviour
+{
+
+    private Animator animator;
+    private SpriteRenderer sprite;
+    private enum animState {witch_idle, witch_walk};
+    private string currentState = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void updateAnimation(float dirX)
+    {
+        string state;
+        Debug.Log("Updated!");
+        if (dirX>0){
+            sprite.flipX= false;
+            state=nameof(animState.witch_walk);
+        } else if (dirX<0){
+            sprite.flipX=true;
+            state=nameof(animState.witch_walk);
+        } else {
+            state=nameof(animState.witch_idle);
+        }
+        changeAnimationState(state);
+    }
+
+    private void changeAnimationState(string newState)
+    {
+        if (currentState==newState) return;
+
+        animator.Play(newState);
+
+        currentState=newState;
+    }
+}
--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -7,26 +7,28 @@ public class player_movement : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody2D rb;
     [SerializeField] private float moveSpeed;
+    player_animation_controller animControllerScript;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Debug.Log("Hunter is stinky");
+        animControllerScript = GetComponent<player_animation_controller>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         bool moving = Input.GetButton("Horizontal") || Input.GetButton("Vertical");
+        float movementX = Input.GetAxisRaw("Horizontal");
+        float movementY = Input.GetAxisRaw("Vertical");
 
         if (moving)
         {
-            float movementX = Input.GetAxisRaw("Horizontal");
-            float movementY = Input.GetAxisRaw("Vertical");
             rb.velocity = new Vector2(movementX, movementY).normalized*moveSpeed;
         }else
         {
             rb.velocity= new Vector2(0,0);
         }
+        animControllerScript.updateAnimation(movementX);
     }
 
 }
