154ba5dd37e40c5d940e06aeaf7a389bd71cc132
Waseem Munir <xxwaseemmunirxx@gmail.com>
2023-07-24 12:59:33 -0700

    Bugged ( Flashes White,Doesn't spawn Zombies,Coin)

---
+++ Assets/Scripts/CoinDrop.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class CoinDrop : MonoBehaviour
+//{
+    //public GameObject CoinModel;
+    //public Transform transform;
+//}/
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -10,7 +10,22 @@ public class Health : MonoBehaviour
 
     [SerializeField]
     private bool isDead = false;
-
+    private Material matWhite;
+    private Material matDefault;
+    private UnityEngine.Object explosionRef;
+    SpriteRenderer sr;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        matWhite = Resources.Load("WhiteFlash", typeof(Material)) as Material;
+        matDefault = sr.material;
+        explosionRef = Resources.Load("Explosion");
+    }
+    //void DropCoin()
+    //{
+//        Vector2 position = transform.position;
+        //GameObject coin = Instantiate(Coin, position,Quaternion.identity);
+   // }
     public void InitializeHealth(int healthValue)
     {
         currentHealth = healthValue;
@@ -21,13 +36,26 @@ public class Health : MonoBehaviour
     public void GetHit(int amount)
     {
         if (isDead)
+        {
             return;
-        currentHealth -= amount;
-
+            currentHealth -= amount;
+            sr.material = matWhite;
+        }
+        else
+        {
+            Invoke("ResetMaterial", .1f);
+        }
         if (currentHealth <= 0)
         {
             isDead = true;
             Destroy(gameObject);
+            GameObject explosion = (GameObject)Instantiate(explosionRef);
+            explosionRef.transform.position = new Vector2(transform.position.x,transform.position.y + .3f,);
+            //ropCoin();
         }
+    }
+    void ResetMaterial()
+    {
+        sr.material = matDefault;
     }
 }
