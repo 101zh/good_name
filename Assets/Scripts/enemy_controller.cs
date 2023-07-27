c8899a6f8a907a6c07b9d1d98c7e6c69fb0fde2a
Hunter Zhuang <hunterzhuang0@gmail.com>
2023-07-27 10:43:49 -0700

    feat: enemy movement finished

--- Assets/Scripts/enemy_controller.cs
+++ Assets/Scripts/enemy_controller.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 public class enemy_movement : MonoBehaviour
 {
 
-    [SerializeField] private float enemyWalkSp;
+    [SerializeField] private float movementSpeed;
     private GameObject player;
     private Rigidbody2D enemyrb;
     [SerializeField] private float attackRange = 5;
     [SerializeField] private float dangerRange = 2.5f;
-    [SerializeField] Vector3 desiredPos;
-    private enum movementState { chase, strafe, runAway };
-    [SerializeField] movementState currentMovementState;
-    float minStrafe;
-    float maxStrafe;
-
+    Vector3 desiredPos;
+    private enum movementState { chase, safe, runAway };
+    movementState currentMovementState;
 
     // Start is called before the first frame update
     void Start()
@@ -23,24 +20,14 @@ public class enemy_movement : MonoBehaviour
         player = GameObject.FindWithTag("Player");
         enemyrb = GetComponent<Rigidbody2D>();
         desiredPos = transform.position;
-        minStrafe = -4;
-        maxStrafe = 4;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         currentMovementState = movementLogic();
-        float movementSpeed = enemyWalkSp * Time.deltaTime;
-        if (currentMovementState == movementState.strafe && desiredPos == enemyrb.transform.position)
-        {
-            desiredPos = pickPosition(currentMovementState);
-            Debug.Log(desiredPos.ToString()+"; "+enemyrb.transform.position.ToString());
-        }
-        else
-        {
-            desiredPos = pickPosition(currentMovementState);
-        }
+        movementSpeed = movementSpeed * Time.deltaTime;
+        desiredPos = pickPosition(currentMovementState);
 
         if (currentMovementState == movementState.runAway)
         {
@@ -51,13 +38,6 @@ public class enemy_movement : MonoBehaviour
             movementSpeed = Mathf.Abs(movementSpeed);
         }
 
-        // Vector2 dir= enemyrb.position;
-        // dir.x=Mathf.MoveTowards(enemyrb.position.x, desiredPos.x, movementSpeed);
-        // dir.y=Mathf.MoveTowards(enemyrb.position.y, desiredPos.y, movementSpeed);
-
-        // enemyrb.MovePosition(dir);
-
-        Debug.Log(movementSpeed);
         enemyrb.transform.position = Vector2.MoveTowards(enemyrb.transform.position, desiredPos, movementSpeed);
 
     }
@@ -66,7 +46,7 @@ public class enemy_movement : MonoBehaviour
     {
         float distanceFromPlayer = Vector2.Distance(player.transform.position, enemyrb.transform.position);
 
-        movementState state = movementState.strafe;
+        movementState state = movementState.safe;
 
         if (distanceFromPlayer >= attackRange)
         {
@@ -74,7 +54,7 @@ public class enemy_movement : MonoBehaviour
         }
         else if (distanceFromPlayer >= dangerRange)
         {
-            state = movementState.strafe;
+            state = movementState.safe;
         }
         else if (distanceFromPlayer <= dangerRange)
         {
@@ -92,16 +72,13 @@ public class enemy_movement : MonoBehaviour
         {
             point = player.transform.position;
         }
-        else if (state == movementState.strafe)
+        else if (state == movementState.safe)
         {
-            float randX = Random.Range(minStrafe, maxStrafe);
-            float randY = Random.Range(minStrafe, maxStrafe);
-            point = new Vector2(enemyrb.transform.position.x + randX, enemyrb.transform.position.y + randY);
-
+            point = transform.position;
         }
         else if (state == movementState.runAway)
         {
-            point = new Vector2(player.transform.position.x, player.transform.position.y);
+            point = player.transform.position;
         }
 
         return point;
