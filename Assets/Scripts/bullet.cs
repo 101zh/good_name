698c7e1d3bda53e8802abff1a98e0d6340c269e2
Hunter Zhuang <hunterzhuang0@gmail.com>
2023-07-31 15:05:33 -0700

    feat(bullet): impact animation code completed

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     Health healthscript;
+    Animator animator;
+    Rigidbody2D rb;
+
+    void Start(){
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
@@ -13,7 +21,10 @@ public class bullet : MonoBehaviour
             healthscript.GetHit(1);
             Debug.Log("I've been hit!");
         }
-        Destroy(gameObject);
+        rb.constraints = RigidbodyConstraints2D.FreezePosition;
+        animator.Play("bullet_impact");
+        Destroy(gameObject, 0.4f);
     }
+
 }
 
